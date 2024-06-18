ee5b65c1502af11899b50eec8fe999be67498f37
ParticleSWE <ell@particle.se>
2024-06-18 14:24:42 +0200

    Initial Commit

---
+++ DevWarsEP2/DevWarsEP2App.swift
@@ -0,0 +1,17 @@
+//
+//  DevWarsEP2App.swift
+//  DevWarsEP2
+//
+//  Created by Patrik Ell on 2024-06-18.
+//
+
+import SwiftUI
+
+@main
+struct DevWarsEP2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
